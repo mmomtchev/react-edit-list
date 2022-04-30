90971ca29c0175ee0269eaa8b090282134d5d248
Momtchil Momtchev <momtchil@momtchev.com>
2022-04-30 18:07:06 +0200

    use React 17 compat mode

--- examples/index.tsx
+++ examples/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
