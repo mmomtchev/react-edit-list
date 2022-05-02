16c36efcf58e8591093022d1064604a061537147
Momtchil Momtchev <momtchil@momtchev.com>
2022-05-02 20:52:33 +0200

    expand the field type description

--- src/base.ts
+++ src/base.ts
@@ -2,6 +2,14 @@ import React from 'react';
 
 /**
  * Field type
+ *
+ * `id` means a hidden element that will be carried on by react-edit-list without any processing
+ *
+ * `string` and `number` have default rendering and input components
+ *
+ * `custom` allows you to define your own rendering and input components
+ *
+ * Passing an array of `name`/`value` pairs allows defining of an enum field
  */
 export type Element =
     | 'id'
