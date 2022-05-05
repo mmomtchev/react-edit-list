5b40aba488edf9295fa4389eb5152e4a306a64a1
Momtchil Momtchev <momtchil@momtchev.com>
2022-05-05 16:22:25 +0200

    enable React 18 rendering of the examples (#1)
    auto-detect React 17 / 18 and adjust the rendering method

--- examples/index.tsx
+++ examples/index.tsx
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+// eslint-disable-next-line no-console
+console.debug('React 16/17 mode');
 ReactDOM.render(<App />, document.getElementById('root'));
